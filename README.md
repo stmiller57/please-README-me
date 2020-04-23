091e1a837ec7b2cbd264fcfb9e8e5864e47a5553
Reformed Scribe <stmiller57@gmail.com>
2020-04-23 13:30:19 -0400

    Created a list of questions whose answers will generate a README.md file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# please-README-me
+# Please README me
+  ## Project description
+  The purpose of this project was to create a README.md generator via node.JS. 
+  ### Nitty-gritty details
+  MIT
+  npm install
+  No tests are needed at this time.
+  Beats me
+  Any contributions and improvements to the code are welcome.
+  
--- index.js
+++ index.js
@@ -1,12 +1,91 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
-];
+const README = fs.writeFile;
 
-function writeToFile(fileName, data) {
+function askQuestions() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
+        {
+            type: "input",
+            name: "urlAddress",
+            message: "What is the URL for your project?"
+        },
+        {
+            type: "input",
+            name: "projectName",
+            message: "What is your project name?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a short description of your project."
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "What type of license should your project have?"
+        },
+        {
+            type: "input",
+            name: "commandInstall",
+            message: "What command should be run to install dependencies?"
+        },
+        {
+            type: "input",
+            name: "commandTest",
+            message: "What command should be run to run tests?"
+        },
+        {
+            type: "input",
+            name: "userKnowledge",
+            message: "What does the user need to know about using the repo?"
+        },
+        {
+            type: "input",
+            name: "contribute",
+            message: "What does the user need to know about contributing to the repo?"
+        }
+    ]);
 }
 
-function init() {
-
+function generateMarkdown(responses) {
+    return `# ${responses.projectName}
+  ## Project description
+  ${responses.description}
+  ### Nitty-gritty details
+  ${responses.license}
+  ${responses.commandInstall}
+  ${responses.commandTest}
+  ${responses.userKnowledge}
+  ${responses.contribute}
+  `;
 }
 
+module.exports = generateMarkdown;
+
+
+async function init() {
+    console.log("Hello")
+    try {
+        const responses = await askQuestions();
+
+        const readme = generateMarkdown(responses);
+
+        await README("README.md", readme);
+
+        console.log("README created!");
+    } catch (err) {
+        console.log(err);
+    }
+}
 init();
